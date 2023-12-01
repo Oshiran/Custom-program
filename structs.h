ee849d5ef147c475023f0dc2c38a19ac80e69c14
Alison Dwyne <102777652@students.swinburne.edu.my>
2023-12-01 20:43:25 +0800

    Struct.h Update

--- structs.h
+++ structs.h
@@ -15,5 +15,5 @@ void BTotal(struct Burger* B, const float price[]);
 void BAmt(struct Burger* B, const float prices[]);
 void MenuTotal(struct MainDish* md, struct Sides* sd, struct Drinks* dk, struct Burger* B);
 void Recipt(struct MainDish* md, struct Sides* sd, struct Drinks* dk, struct Burger* B);
-
+void readTextFile();
 #endif
